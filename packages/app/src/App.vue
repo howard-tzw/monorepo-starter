5d90476bf0c8a984c389284372c78ba5b2e5d4ab
Howard <howard.tzw@gmail.com>
2023-10-02 10:30:49 +0800

    re-config

---
+++ packages/app/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { greeting, foo } from '@monorepo/utils'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+	greeting()
+})
+</script>
+
+<template>
+	<div>Playground {{ foo }}</div>
+</template>
